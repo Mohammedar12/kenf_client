1351c29b162b52ee23d129363e6e517274b46589
Ali <91572113+Mohammedar12@users.noreply.github.com>
2023-01-07 02:42:11 +0300

    Update index.js

--- containers/exclusiveProducts/index.js
+++ containers/exclusiveProducts/index.js
@@ -8,32 +8,32 @@ const ExclusiveProducts = props => {
 
     const { t } = useTranslation();
 
-    useEffect(() => {
-        $(".exclusive-carousel").owlCarousel({
-            center: true,
-            loop: true,
-            margin: 10,
-            autoplay: true,
-            autoplaySpeed: 1000,
-            dragEndSpeed: 800,
-            nav: false,
-            dots: false,
-            responsive: {
-                0: {
-                    items: 2,
-                },
-                600: {
-                    items: 2,
-                },
-                700: {
-                    items: 3,
-                },
-                1000: {
-                    items: 3,
-                },
-            },
-        });
-    }, []);
+//     useEffect(() => {
+//         $(".exclusive-carousel").owlCarousel({
+//             center: true,
+//             loop: true,
+//             margin: 10,
+//             autoplay: true,
+//             autoplaySpeed: 1000,
+//             dragEndSpeed: 800,
+//             nav: false,
+//             dots: false,
+//             responsive: {
+//                 0: {
+//                     items: 2,
+//                 },
+//                 600: {
+//                     items: 2,
+//                 },
+//                 700: {
+//                     items: 3,
+//                 },
+//                 1000: {
+//                     items: 3,
+//                 },
+//             },
+//         });
+//     }, []);
 
     return (
         <section className="exclusive pt-md-3 pb-md-3 pt-1 pb-1 position-relative ">
