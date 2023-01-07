48cde8224433797e0356b30616a8eeba765d502c
Ali <91572113+Mohammedar12@users.noreply.github.com>
2023-01-07 15:36:45 +0300

    Update index.js

--- components/banner_panel/index.js
+++ components/banner_panel/index.js
@@ -4,6 +4,18 @@ import { useResize } from '../../utils/helper';
 import Images from '../image_panel';
 import "animate.css"
 
+import dynamic from "next/dynamic";
+import { fx } from "jquery";
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
+const responsive = {
+  0: {
+    items: 1,
+  },
+};
+
 const Banner = () => {
     const { width } = useResize();
 
@@ -12,24 +24,49 @@ const Banner = () => {
 //     }, []);
 
     return (
-        <section className="banners animate__animated animate__fadeInUp">
-            <div className="banners-main owl-carousel owl-theme ">
-                <div className="item">
-                    {
-                        width <= 700 ?
-                            <Images src="images/bannn_mob_1.png" classnames="d-block img-fluid banners-mobail" alt="..." /> :
-                            <Images src="images/banner1.png" classnames="d-block img-fluid banners-descktop" alt="..." />
-                    }
-                </div>
-                <div className="item">
-                    {
-                        width <= 700 ?
-                            <Images src="images/bannn_mob_2.png" classnames="d-block img-fluid banners-mobail" alt="..." /> :
-                            <Images src="images/banner2.png" classnames="d-block img-fluid banners-descktop" alt="..." />
-                    }
-                </div>
-            </div>
-        </section>
+        <OwlCarousel
+        className="banners-main owl-theme  "
+        loop
+        margin={10}
+        nav
+        navSpeed={1000}
+        autoplay={true}
+        autoplaySpeed={2000}
+        dotsSpeed={800}
+        dragEndSpeed={800}
+        responsive={responsive}
+      >
+        <div className="item">
+          {width <= 700 ? (
+            <Images
+              src="images/bannn_mob_1.png"
+              classnames="d-block img-fluid banners-mobail"
+              alt="..."
+            />
+          ) : (
+            <Images
+              src="images/banner1.png"
+              classnames="d-block img-fluid banners-descktop"
+              alt="..."
+            />
+          )}
+        </div>
+        <div className="item">
+          {width <= 700 ? (
+            <Images
+              src="images/bannn_mob_2.png"
+              classnames="d-block img-fluid banners-mobail"
+              alt="..."
+            />
+          ) : (
+            <Images
+              src="images/banner2.png"
+              classnames="d-block img-fluid banners-descktop"
+              alt="..."
+            />
+          )}
+        </div>
+      </OwlCarousel>
     )
 }
 
