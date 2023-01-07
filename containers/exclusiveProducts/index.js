64eeac57cfad059a1c1fee6832b9c60680585e8d
Ali <91572113+Mohammedar12@users.noreply.github.com>
2023-01-07 15:34:38 +0300

    fix loading issues

--- containers/exclusiveProducts/index.js
+++ containers/exclusiveProducts/index.js
@@ -3,6 +3,12 @@ import React, { useEffect } from 'react';
 import ExclusiveCard from '../../components/exclusive_card';
 import { useTranslation } from 'react-i18next';
 
+import dynamic from "next/dynamic";
+import { fx } from "jquery";
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
+
 const ExclusiveProducts = props => {
     const { datas } = props;
 
@@ -34,18 +40,42 @@ const ExclusiveProducts = props => {
 //             },
 //         });
 //     }, []);
+    
+    const responsive = {
+  0: {
+    items: 2,
+  },
+  600: {
+    items: 2,
+  },
+  700: {
+    items: 3,
+  },
+  1000: {
+    items: 3,
+  },
+};
+
 
     return (
-        <section className="exclusive pt-md-3 pb-md-3 pt-1 pb-1 position-relative ">
-            <h3 className="section-title">{t('exclusive_products')}</h3>
-            <div className="exclusive-carousel owl-carousel owl-theme ">
-                {
-                    datas.filter(item => item.isExclusive && !item.deleted && !item.hidden).map((item, index) => (
-                        <ExclusiveCard key={index} data={item} />
-                    ))
-                }
-            </div>
-        </section>
+  <OwlCarousel
+        className="exclusive-carousel owl-theme "
+        responsive={responsive}
+        center={true}
+        loop
+        margin={10}
+        autoplay={true}
+        autoplaySpeed={1000}
+        dragEndSpeed={800}
+        nav={false}
+        dots={false}
+      >
+        {datas
+          .filter((item) => item.isExclusive && !item.deleted && !item.hidden)
+          .map((item, index) => (
+            <ExclusiveCard key={index} data={item} />
+          ))}
+      </OwlCarousel>
     )
 }
 
