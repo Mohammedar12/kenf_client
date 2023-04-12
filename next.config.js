83ddb6ab780e712d0874139d766c8b5a56baaa93
CodeSoul-git <67316318+CodeSoul-git@users.noreply.github.com>
2023-04-12 15:58:29 +0530

    New update -v 1

--- next.config.js
+++ next.config.js
@@ -8,9 +8,8 @@ const nextConfig = {
     loader: "default",
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
+        protocol: 'https',
+        hostname: 'kenf.sa',
         pathname: '/**',
       },
     ],
