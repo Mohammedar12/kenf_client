2d207635a577f64c2a0130ac07a0d7554673713d
Ali <91572113+Mohammedar12@users.noreply.github.com>
2023-06-10 21:52:25 +0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const ServerURI = "http://localhost:3000/api";
-export const imageURI = "http://localhost:3000";
-// export const imageURI = "https://kenf.sa";
-// export const ServerURI = "https://kenf.sa/api";
+// export const ServerURI = "http://localhost:3000/api";
+// export const imageURI = "http://localhost:3000";
+export const imageURI = "https://kenf.sa";
+export const ServerURI = "https://kenf.sa/api";
